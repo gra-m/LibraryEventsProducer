cb4dbee3e5051fd5a0019a1ff963cb04efba4f86
gra-m <grahamd483@gmail.com>
2023-04-13 14:04:07 +0200

    @Parameter resolution error looking at other examples of Embedded Kafka Integration tests

---
+++ src/test/java/fun/madeby/controller/IntLibEventControllerTest.java
@@ -0,0 +1,96 @@
+package fun.madeby.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import fun.madeby.domain.Book;
+import fun.madeby.domain.LibEvent;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
+
+// avoid conflict with 8080 by using random // TestPropSource overrides the paths for kafka brokers
+// in application-local.yml
+
+@SpringBootTest(
+    classes = {EmbeddedKafkaBroker.class, ServletWebServerFactoryAutoConfiguration.class},
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(
+    topics = {"lib-events"},
+    partitions = 3, controlledShutdown = true)
+@TestPropertySource(
+    properties = {
+      "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+      "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"
+    })
+public class IntLibEventControllerTest {
+@Autowired
+  private EmbeddedKafkaBroker embeddedKafkaBroker;
+  private TestRestTemplate restTemplate; // auto maps to RANDOM_PORT
+  private Consumer<Integer, String> consumer;
+
+  @Autowired
+  IntLibEventControllerTest(EmbeddedKafkaBroker embeddedKafkaBroker, TestRestTemplate trt) {
+    this.restTemplate = trt;
+  }
+
+  @BeforeEach
+  void setUp() {
+    Map<String, Object> configs =
+        new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+    Consumer<Integer, String> consumer =
+        new DefaultKafkaConsumerFactory<>(
+                configs, new IntegerDeserializer(), new StringDeserializer())
+            .createConsumer();
+    embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+  }
+
+  @AfterEach
+  void tearDown() {
+    consumer.close();
+  }
+
+  @Test
+  @Timeout(5)
+  void postLibEvent() throws InterruptedException {
+    // given
+
+    Book book =
+        Book.builder().bookId(123).bookAuthor("Dilip").bookName("Kefku using Sprang Boat").build();
+
+    LibEvent libEvent = LibEvent.builder().libraryEventId(null).book(book).build();
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("content-type", MediaType.APPLICATION_JSON.toString());
+    HttpEntity<LibEvent> request = new HttpEntity<>(libEvent, headers);
+
+    // when
+    ResponseEntity<LibEvent> responseEntity =
+        restTemplate.exchange("/v2/lib-event", HttpMethod.POST, request, LibEvent.class);
+
+    // then
+    assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    ConsumerRecord<Integer, String> consumerRecord =
+        KafkaTestUtils.getSingleRecord(consumer, "lib-events");
+    // Thread.sleep(3000);
+    String expectedRecord =
+        "{\"libraryEventId\":null,\"libraryEventType\":\"NEW\",\"book\":{\"bookId\":456,\"bookName\":\"Kafka Using Spring Boot\",\"bookAuthor\":\"Dilip\"}}";
+    assertEquals(expectedRecord, consumerRecord.value());
+  }
+}
